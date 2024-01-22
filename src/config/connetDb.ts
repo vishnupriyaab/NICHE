21be043c52c19a363b01c6effd814503f3630a4b
vishnupriyaab <vishnupriyaotp2002@gmail.com>
2024-01-22 16:31:48 +0530

    folderStructed

--- src/config/connetDb.ts
+++ src/config/connetDb.ts
@@ -1,1 +1,11 @@
 import mongoose from "mongoose";
+const url = process.env.DATABASE_URL || "mongodb://localhost:27017/NIHCE_Backup"
+
+function connectionDB(){
+    mongoose.set('strictQuery',true);
+    mongoose
+    .connect(url)
+    .then(()=>console.log("database connected", url))
+    .catch((error)=>console.error(error));
+}
+export default connectionDB;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
+import "dotenv/config";
 import express, { Application,Request,Response } from "express";
+import connectionDB from "./config/connetDb";
 
 const app:Application = express();
-
 app.use((req:Request,res:Response)=>{
   res.send("okkkk");
 })
-
+connectionDB();
 const port:number =3000;
 app.listen(port, () => {
   console.log("server listening..");
