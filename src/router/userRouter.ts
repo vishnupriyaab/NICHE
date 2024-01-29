eddc51c9c0b548bf509de68a20a1296bc53345bd
vishnupriyaab <vishnupriyaotp2002@gmail.com>
2024-01-29 20:03:31 +0530

    confirmPassword

--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { getLogin, userRegister } from "../controller/userController";
-// import {userRegistrationValidation , validateUserRegistration} from "../middleware/userAuth";
+import {userRegistrationValidation , validateUserRegistration} from "../middleware/userAuth";
 
 const userRoute: Router = Router();
 
 userRoute.get("/userLogin", getLogin);
 
-userRoute.post("/registerValue" , userRegister);
+userRoute.post("/registerValue", userRegistrationValidation,validateUserRegistration , userRegister);
 
 export default userRoute;
